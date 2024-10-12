f0e97a6119aac26eac2e5816b781a0f62298771d
yotsubo <513047.otsubo@gmail.com>
2024-10-13 02:45:52 +0900

    [step3] add: minishell.h

--- include/minishell.h
+++ include/minishell.h
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/13 01:47:03 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/10/13 02:41:53 by yuotsubo         ###   ########.fr       */
+/*   Created: 2024/10/13 02:40:54 by yuotsubo          #+#    #+#             */
+/*   Updated: 2024/10/13 02:45:07 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/minishell.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
 
-int	main(void)
-{
-	char	*line;
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
 
-	rl_outstream = stderr;
-	while (1)
-	{
-		line = readline("minishell$ ");
-		if (!line)
-			break ;
-		if (*line)
-			add_history(line);
-		// TODO: intepret line as a command
-		free(line);
-	}
-	exit(EXIT_SUCCESS);
-}
+#endif
