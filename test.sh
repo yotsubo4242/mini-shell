bfc194970acf31e1a610bb66aa74e09e83b53e13
yotsubo <513047.otsubo@gmail.com>
2024-10-13 03:57:58 +0900

    [step4] modified: add command name & executable file tests

--- test.sh
+++ test.sh
@@ -32,3 +32,19 @@ assert 'hello'
 # absolute path commands
 assert '/bin/pwd'
 assert '/bin/echo'
+
+# Generate Executable
+cat <<EOF | gcc -xc -o a.out -
+#include <stdio.h>
+int main() { printf("hello from a.out\n"); }
+EOF
+
+# Search command path without args
+assert 'pwd'
+assert 'echo'
+assert 'ls'
+assert './a.out'
+
+## no such command
+assert 'a.out'
+assert 'nosuchfile'
