b31b5c24a65484f0e324a8beafc6f1a452492606
yotsubo <513047.otsubo@gmail.com>
2024-10-14 13:59:24 +0900

    [step5] add: struct.h

--- include/struct.h
+++ include/struct.h
@@ -1,30 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/13 02:40:54 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/10/14 13:58:57 by yuotsubo         ###   ########.fr       */
+/*   Created: 2024/10/14 13:54:38 by yuotsubo          #+#    #+#             */
+/*   Updated: 2024/10/14 13:57:48 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#ifndef STRUCT_H
+# define STRUCT_H
 
-# include "libft.h"
-# include "struct.h"
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <string.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <sys/wait.h>
+typedef enum	e_token_kind {
+	TK_WORD,
+	TK_RESERVED,
+	TK_OP,
+	TK_EOF,
+} t_token_kind;
 
-int		interpret(char *line);
-
-char	*search_path(const char *filename);
+typedef struct s_token {
+	char			*word;
+	t_token_kind	kind;
+	struct s_token	*next;
+}	t_token ;
 
 #endif
