cb6b67e211c815df41bd5c94545163010db94b2c
yotsubo <513047.otsubo@gmail.com>
2024-10-16 20:22:44 +0900

    [step9] modified: struct.h: add new token & node to adjust redirect

--- include/struct.h
+++ include/struct.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yotsubo <y.otsubo.886@ms.saitama-u.ac.j    +#+  +:+       +#+        */
+/*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 13:54:38 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/10/16 00:19:54 by yotsubo          ###   ########.fr       */
+/*   Updated: 2024/10/16 20:20:49 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,14 @@ typedef enum	e_token_kind {
 	TK_RESERVED,
 	TK_OP,
 	TK_EOF,
+	TK_RRID,
+	TK_LRID,
 } t_token_kind;
 
 typedef enum	e_node_kind {
 	ND_SIMPLE_CMD,
+	ND_RRID,
+	ND_LRID,
 } t_node_kind;
 
 typedef struct s_token {
