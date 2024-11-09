a3c42adf220001139c6061a6521a2dcef3d014c1
yotsubo <513047.otsubo@gmail.com>
2024-11-09 14:09:08 +0900

    [step9, 10] ref: interpret.c: comment out interpret(), make_options()

--- src/exec/interpret.c
+++ src/exec/interpret.c
@@ -6,7 +6,7 @@
 /*   By: yotsubo <y.otsubo.886@ms.saitama-u.ac.j    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 02:34:47 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/11/09 02:56:27 by yotsubo          ###   ########.fr       */
+/*   Updated: 2024/11/09 14:08:20 by yotsubo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,57 +31,55 @@ size_t	get_param_num(t_token *tok)
 	return (param_num);
 }
 
-char	**make_options(t_token *tok)
-{
-	char	**cmd_with_opt;
-	size_t	param_num;
-	size_t	i;
+// char	**make_options(t_token *tok)
+// {
+// 	char	**cmd_with_opt;
+// 	size_t	param_num;
+// 	size_t	i;
 
-	param_num = get_param_num(tok);
-	cmd_with_opt = (char **)malloc(sizeof(char *) * (param_num + 1));
-	if (!cmd_with_opt)
-		return (NULL);
-	i = 0;
-	while (i < param_num)
-	{
-		cmd_with_opt[i++] = tok->word;
-		tok = tok->next;
-	}
-	cmd_with_opt[i] = NULL;
-	return (cmd_with_opt);
-}
+// 	param_num = get_param_num(tok);
+// 	cmd_with_opt = (char **)malloc(sizeof(char *) * (param_num + 1));
+// 	if (!cmd_with_opt)
+// 		return (NULL);
+// 	i = 0;
+// 	while (i < param_num)
+// 	{
+// 		cmd_with_opt[i++] = tok->word;
+// 		tok = tok->next;
+// 	}
+// 	cmd_with_opt[i] = NULL;
+// 	return (cmd_with_opt);
+// }
 
-int	interpret(t_node *node)
-{
-	extern char	**environ;
-	char		**cmd_with_opt;
-	pid_t		pid;
-	int			wstatus;
-	char		*cmd;
+// int	interpret(t_node *node)
+// {
+// 	extern char	**environ;
+// 	char		**cmd_with_opt;
+// 	pid_t		pid;
+// 	int			wstatus;
+// 	char		*cmd;
 
-	pid = fork();
-	if (pid < 0)
-		fatal_error("fork");
-	else if (!pid)
-	{
-		// child process
-		cmd = search_path(node->args->word);
-		if (!cmd)
-			fatal_error("command not found");
-		cmd_with_opt = make_options(node->args);
-		execve(cmd, cmd_with_opt, environ);
-		fatal_error("execve");
-	} else {
-		// parent process
-		wait(&wstatus);
-		return (WEXITSTATUS(wstatus));
-	}
-}
+// 	pid = fork();
+// 	if (pid < 0)
+// 		fatal_error("fork");
+// 	else if (!pid)
+// 	{
+// 		// child process
+// 		cmd = search_path(node->args->word);
+// 		if (!cmd)
+// 			fatal_error("command not found");
+// 		cmd_with_opt = make_options(node->args);
+// 		execve(cmd, cmd_with_opt, environ);
+// 		fatal_error("execve");
+// 	} else {
+// 		// parent process
+// 		wait(&wstatus);
+// 		return (WEXITSTATUS(wstatus));
+// 	}
+// }
 
 
 // 宇佐美さんのpipeline時点から引用.
-
-
 
 // これは一旦自作.
 void	err_exit(const char *name, const char *err_msg, int estatus)
