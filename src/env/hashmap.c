2761ebf9527b57aadb1883883e0701591b58a154
yotsubo <513047.otsubo@gmail.com>
2024-11-14 07:28:38 +0900

    [step13] add: hashmap.c: map_set() & map_set() utils

--- src/env/hashmap.c
+++ src/env/hashmap.c
@@ -6,13 +6,14 @@
 /*   By: yotsubo <y.otsubo.886@ms.saitama-u.ac.j    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 21:33:25 by yotsubo           #+#    #+#             */
-/*   Updated: 2024/11/14 07:01:15 by yotsubo          ###   ########.fr       */
+/*   Updated: 2024/11/14 07:27:39 by yotsubo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "env.h"
 
+// cleanup関数群
 void	cleanup_item(t_item *item)
 {
 	if (item == NULL)
@@ -40,6 +41,7 @@ void	cleanup_map(t_map *map)
 	free(map);
 }
 
+// new関数群
 // 渡されてきたkey, valueはdupしてitemの中に格納する. 
 t_item	*item_new(char *key, char *value)
 {
@@ -71,6 +73,7 @@ t_map *map_new(void)
 	return (map);
 }
 
+// map_get
 // keyに対応するvalueを得る関数. 戻り値は辞書上のvalueのアドレス. 
 // 辞書にkeyが存在しない場合はNULLを返す.  
 // 一旦NULLガードはしないので, 呼び出し元でmap, keyの存在は保証する. 
@@ -87,4 +90,88 @@ char	*map_get(t_map *map, const char *key)
 		item = item->next;
 	}
 	return (NULL);
+}
+
+// map_set関数群
+t_bool	is_alpha_under(char c)
+{
+	return (c == '_' || ft_isalpha(c));
+}
+
+t_bool	is_alnum_under(char c)
+{
+	return (is_alpha_under(c) || ft_isdigit(c));
+}
+
+t_bool	is_identifier(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!is_alpha_under(str[i]))
+		return (FALSE);
+	i++;
+	while (str[i])
+	{
+		if (!is_alnum_under(str[i]))
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
+
+void	item_update(t_item *item, const char *value)
+{
+	free(item->value);
+	if (value == NULL)
+		item->value = NULL;
+	else
+	{
+		item->value = ft_strdup(value);
+		if (item->value == NULL)
+			fatal_error ("strdup");
+	}
+}
+
+t_item	*item_apend(t_item *item, const char *key, const char *value)
+{
+	t_item	*item;
+
+	if (value == NULL)
+	{
+		item = item_new(ft_strdup(key), NULL);
+		if (item->key == NULL)
+			fatal_error("strdup");
+	} else {
+		item = item_new(ft_strdup(key), ft_strdup(value));
+		if (item->key == NULL || item->value == NULL)
+			fatal_error ("strdup");
+	}
+	return (item);
+}
+
+// keyとvalueを元に, mapの(更新 || 追加)を行う関数. 
+// keyが存在していれば更新, 存在していなければ追加を行う. 
+// 処理が正常に終わったときは0, エラーの時は-1を返す. 
+// エラー：keyがNULLか環境変数のの命名規則に合っていないもの.
+int	map_set(t_map *map, const char *key, const char *value)
+{
+	t_item	*item;
+	t_item	*prev;
+
+	if (key == NULL || !is_identifier(key))
+		return (-1);
+	item = map->item_head.next;
+	while (item != NULL)
+	{
+		if (!ft_strncmp(key, item->key, ft_strlen(key)))
+			break ;
+		prev = item;
+		item = item->next;
+	}
+	if (item != NULL)
+		item_update(item, value);
+	else
+		prev->next = item_apend(item, key, value);
+	return (0);
 }
