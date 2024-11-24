adfbf88ed2c83231e8181cb059efaee483d8f8c5
tor-ato <toratokitahara@gmail.com>
2024-11-24 14:21:24 +0900

    [TEST] mod: add color to ok and ng

--- test.sh
+++ test.sh
@@ -1,28 +1,34 @@
 assert() {
-	# テストしようとしている内容をprint
-	printf '%-30s:' "\"$1\""
+    # 色の定義
+    GREEN='\033[32m'
+    RED='\033[31m'
+    RESET='\033[0m'
 
-	# bashの出力をcmpに保存
-	echo -n -e "$1" | bash >cmp 2>&-
-	# bashのexit statusをexpectedに代入
-	expected=$?
-	# minishellの出力をoutに保存
-	echo -n -e "$1" | ./minishell >out 2>&-
-	# minishellのexit statusをactualに代入
-	actual=$?
+    # テストしようとしている内容をprint
+    printf '%-30s:' "\"$1\""
 
-	# bashとminishellの出力を比較
-	diff cmp out >/dev/null && echo -n '  diff ok' || echo -n '  diff ng'
+    # bashの出力をcmpに保存
+    echo -n -e "$1" | bash >cmp 2>&-
+    expected=$?
 
-	# bashとminishellのexit statusを比較
-	if [ "$actual" = "$expected" ]; then
-		echo -n '  status ok'
-	else
-		echo -n "  status ng, expected $expected but got $actual"
-	fi
-	echo
+    # minishellの出力をoutに保存
+    echo -n -e "$1" | ./minishell >out 2>&-
+    actual=$?
+
+    # diffの結果を色付きで表示
+    if diff cmp out >/dev/null; then
+        printf " ${GREEN}diff ok${RESET}"
+    else
+        printf " ${RED}diff ng${RESET}"
+    fi
+
+    # statusの結果を色付きで表示
+    if [ "$actual" = "$expected" ]; then
+        printf " ${GREEN}status ok${RESET}\n"
+    else
+        printf " ${RED}status ng, expected $expected but got $actual${RESET}\n"
+    fi
 }
-
 # empty line (eof)
 assert ''
 
