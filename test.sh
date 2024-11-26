0ea85f8cbb9e8e6c46afd70c343275e491286934
tor-ato <toratokitahara@gmail.com>
2024-11-26 14:24:24 +0900

    [TEST] mod: assert()

--- test.sh
+++ test.sh
@@ -1,19 +1,15 @@
 
 # def color
 RESET='\033[0m'
-
-BLACK='\033[30m'
 RED='\033[31m'
 GREEN='\033[32m'
 YELLOW='\033[33m'
-BLUE='\033[34m'
-MAGENTA='\033[35m'
-CYAN='\033[36m'
 WHITE='\033[37m'
 
 # def OK NG
 OK=$GREEN"OK"$RESET
 NG=$RED"NG"$RESET
+
 
 print_section() {
 	echo -e $CYAN"$1"$RESET
@@ -23,32 +19,42 @@ print_desc() {
 	echo -e $YELLOW"$1"$RESET
 }
 
+cleanup() {
+	rm -f cmp out a.out print_args exit42 infinite_loop no_exec_perm no_read_perm
+}
+
 assert() {
+	COMMAND="$1"
+	shift
+	printf '%-60s:' "[$COMMAND]"
+	# exit status
+	echo -n -e "$COMMAND" | bash >cmp 2>&-
+	expected=$?
+	for arg in "$@"
+	do
+		mv "$arg" "$arg"".cmp"
+	done
+	echo -n -e "$COMMAND" | ./minishell >out 2>&-
+	acutual=$?
+	for arg in "$@"
+	do
+		mv "$arg" " $arg"".out"
+	done
 
-    # テストしようとしている内容をprint
-    printf '%-30s:' "\"$1\""
+	diff cmp out >/dev/null  && echo -e -n " diff $OK" || echo -e -n " diff $NG"
 
-    # bashの出力をcmpに保存
-    echo -n -e "$1" | bash >cmp 2>&-
-    expected=$?
+	if [ "$acutual" = "$expected" ]; then 
+		echo -e -n " status $OK"
+	else
+		echo -e -n " status $NG, expected $expected but got $acutual"
+	fi
 
-    # minishellの出力をoutに保存
-    echo -n -e "$1" | ./minishell >out 2>&-
-    actual=$?
-
-    # diffの結果を色付きで表示
-    if diff cmp out >/dev/null; then
-        printf " diff $OK"
-    else
-        printf " diff $NG"
-    fi
-
-    # statusの結果を色付きで表示
-    if [ "$actual" = "$expected" ]; then
-        printf " status $OK\n"
-    else
-        printf " status $NG, expected $expected but got $actual\n"
-    fi
+	for arg in "$@"
+	do
+		echo -n " [$arg] "
+		diff "$arg"".cmp" "$arg"".out" >/dev/null && echo -e -n "$OK" || echo -e -n "$NG"
+	done	
+	echo
 }
 # empty line (eof)
 assert ''
@@ -139,3 +145,8 @@ assert 'echo $?'
 assert 'invalid\necho $?\necho $?'
 assert 'exit42\necho $?\necho $?'
 assert 'exit42\n\necho $?\necho $?'
+
+
+
+
+cleanup
