ad1b6f1c4cad2d4da5c2ffcf75a0238c77cb81cd
yotsubo <513047.otsubo@gmail.com>
2024-11-29 16:24:28 +0900

    [#35] modified: #58

--- src/exec/search_path.c
+++ src/exec/search_path.c
@@ -6,7 +6,7 @@
 /*   By: yotsubo <y.otsubo.886@ms.saitama-u.ac.j    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 03:08:02 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/11/23 02:15:57 by yotsubo          ###   ########.fr       */
+/*   Updated: 2024/11/29 16:13:03 by yotsubo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ char	*search_path(const char *filename)
 		end = ft_strchr(value, ':');
 		// TODO: ft_strlcpy()
 		if (end)
-			ft_strlcpy(path, value, end - value);
+			ft_strlcpy(path, value, end - value + 1);
 		else
 			ft_strlcpy(path, value, PATH_MAX);
 		ft_strlcat(path, "/", PATH_MAX);
