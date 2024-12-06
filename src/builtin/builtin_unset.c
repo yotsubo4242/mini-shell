b79d6754df4d6bd8b1ae2bd499664a3defbc52f8
tor-ato <toratokitahara@gmail.com>
2024-12-06 15:59:17 +0900

    [#45] done: unset fix

--- src/builtin/builtin_unset.c
+++ src/builtin/builtin_unset.c
@@ -12,9 +12,6 @@
 
 #include "minishell.h"
 
-// unsetは変数の削除を行う. 
-// 存在しない変数が指定されたときはエラー文は表示されない. 
-// identifier以外の文字列が来た場合, エラー文が表示される. 
 int	builtin_unset(char **argv)
 {
 	int		status;
@@ -24,13 +21,8 @@ int	builtin_unset(char **argv)
 	i = 1;
 	while (argv[i])
 	{
-		if (map_unset(g_env, argv[i]) < 0)
-		{
-			ft_dprintf(STDERR_FILENO, "unset: %s: not a valid identifier\n", argv[i]);
-			status = 1;
-		}
-		else
-			status = 0;
+		map_unset(g_env, argv[i]);
+		status = 0;
 		i++;
 	}
 	return (status);
