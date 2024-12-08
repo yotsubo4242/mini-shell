151d8f39337da7c2b408fd70dcfd70a590dbf7b7
tor-ato <toratokitahara@gmail.com>
2024-12-08 17:21:08 +0900

    [#43] done: exit

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -20,19 +20,15 @@ static void	print_and_exit(int status)
 
 static bool	is_numeric(char *s)
 {
-	long long	num;
 
-	if (!isdigit(*s))
+	if (*s == '-' || *s == '+')
+		s++;
+	if (!*s)
 		return (false);
-	num = 0;
 	while (*s)
 	{
 		if (!ft_isdigit(*s))
 			return (false);
-		if (num > LLONG_MAX / 10 || (num == LLONG_MAX / 10 && *s - '0' > LLONG_MAX % 10))
-			return (false);
-		num *= 10;
-		num += *s - '0';
 		s++;
 	}
 	return (true);
@@ -43,9 +39,7 @@ int	builtin_exit(char **argv)
 	long	res;
 	char	*arg;
 
-	if (argv == NULL)
-		print_and_exit(g_last_status);
-	if (argv[1] == NULL)
+	if (argv == NULL || argv[1] == NULL)
 		print_and_exit(g_last_status);
 	if (argv[2])
 	{
@@ -55,9 +49,13 @@ int	builtin_exit(char **argv)
 	arg = argv[1];
 	if (is_numeric(arg))
 	{
-		// TODO is_minus
-		// exit (255);
-		res = ft_atoi(argv[1]);
+		errno = 0;
+		res = ft_strtol(arg);
+		if (errno == ERANGE)
+		{
+			xperror2("exit", "numeric argument required");
+			return (2);
+		}
 		print_and_exit(res);
 	}
 	xperror2("exit", "numeric argument required");
