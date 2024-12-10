5a23a6721200ba44e3f506646de68bc839d33f93
tor-ato <toratokitahara@gmail.com>
2024-12-10 17:07:20 +0900

    [#48] refactor: norm OK

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -6,71 +6,17 @@
 /*   By: yotsubo <y.otsubo.886@ms.saitama-u.ac.j    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 12:30:49 by yotsubo           #+#    #+#             */
-/*   Updated: 2024/12/06 14:32:01 by yotsubo          ###   ########.fr       */
+/*   Updated: 2024/12/10 16:41:06 by tkitahar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int    is_overflow(unsigned long val, unsigned long next, int is_neg)
-{
-    if (is_neg)
-    {
-        if (val > ((unsigned long)LONG_MAX + 1) / 10)
-            return (1);
-        if (val == ((unsigned long)LONG_MAX + 1) / 10
-            && next > ((unsigned long)LONG_MAX + 1) % 10)
-            return (1);
-    }
-    else
-    {
-        if (val > (unsigned long)LONG_MAX / 10)
-            return (1);
-        if (val == (unsigned long)LONG_MAX / 10
-            && next > (unsigned long)LONG_MAX % 10)
-            return (1);
-    }
-    return (0);
-}
-
-static	bool is_plusminus(char s)
+bool	is_plusminus(char s)
 {
 	return (s == '-' || s == '+');
 }
 
-long    ft_strtol(const char *str)
-{
-    unsigned long    val;
-    int             is_neg;
-    long            ret;
-
-    while (*str == ' ' || ft_isspace(*str))
-        str++;
-    is_neg = 0;
-    if (is_plusminus(*str))
-    {
-        if (*str == '-')
-            is_neg = 1;
-        str++;
-    }
-    val = 0;
-    while (ft_isdigit(*str))
-    {
-        if (is_overflow(val, *str - '0', is_neg))
-        {
-            errno = ERANGE;
-            if (is_neg)
-                return (LONG_MIN);
-            return (LONG_MAX);
-        }
-        val = val * 10 + (*str - '0');
-        str++;
-    }
-    ret = (long)val;
-    if (is_neg)
-        ret = -ret;
-    return (ret);
-}
 static void	print_and_exit(int status)
 {
 	ft_dprintf(STDERR_FILENO, "exit\n");
@@ -84,14 +30,13 @@ static bool	is_numeric(char *s)
 	if (is_plusminus(*s))
 		s++;
 	if (!*s)
-		return (false); 
+		return (false);
 	while (*s)
 	{
 		if (ft_isspace(*s))
 			break ;
 		if (!ft_isdigit(*s))
-			// koko
-	 		return (false);
+			return (false);
 		s++;
 	}
 	while (ft_isspace(*s))
