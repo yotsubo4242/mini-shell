07ef3e9ff69619ab2084abc5dd358fc0b81c7e7d
yotsubo <513047.otsubo@gmail.com>
2024-12-13 16:37:10 +0900

    [#121] ref: syntax_error.c: adjust to norm

--- src/error/syntax_error.c
+++ src/error/syntax_error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   syntax_error.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkitahar <tkitahar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 15:59:56 by tkitahar          #+#    #+#             */
-/*   Updated: 2024/12/03 15:59:57 by tkitahar         ###   ########.fr       */
+/*   Updated: 2024/12/13 16:36:28 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	tokenize_error(const char *location, char **rest, char *line)
 {
 	gs_syntax_error(SET, TRUE);
 	perror_prefix();
-	ft_dprintf(STDERR_FILENO, "syntax error near unexpected character `%c' in %s\n", *line, location);
+	ft_dprintf(STDERR_FILENO, \
+		"syntax error near unexpected character `%c' in %s\n", *line, location);
 	while (*line)
 		line++;
 	*rest = line;
@@ -26,7 +27,8 @@ void	parse_error(const char *location, t_token **rest, t_token *tok)
 {
 	gs_syntax_error(SET, TRUE);
 	perror_prefix();
-	ft_dprintf(STDERR_FILENO, "syntax error near unexpected token `%s' in %s\n",tok->word , location);
+	ft_dprintf(STDERR_FILENO, \
+		"syntax error near unexpected token `%s' in %s\n", tok->word, location);
 	while (tok->kind != TK_EOF)
 		tok = tok->next;
 	*rest = tok;
