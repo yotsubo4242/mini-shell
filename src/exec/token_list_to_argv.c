d95abe7d99e6ce2e314632752860e5f9124345b0
tor-ato <toratokitahara@gmail.com>
2024-12-13 18:53:51 +0900

    [#122] ref: norm exec done

--- src/exec/token_list_to_argv.c
+++ src/exec/token_list_to_argv.c
@@ -12,34 +12,38 @@
 
 #include "minishell.h"
 
+char	**dup_is_expanded(char **argv, size_t i, t_token *tok)
+{
+	bool	is_word_head;
+	size_t	j;
+
+	is_word_head = true;
+	j = 0;
+	while (j < ft_strlen(tok->word))
+	{
+		if (ft_isspace(tok->word[j]))
+			is_word_head = true;
+		else if (is_word_head)
+		{
+			is_word_head = false;
+			argv[i] = dup_until_space(tok->word + j);
+		}
+		j++;
+	}
+	return (argv);
+}
+
 char	**token_list_to_argv(t_token *tok)
 {
 	char	**argv;
 	size_t	i;
-	size_t	j;
-	bool	is_word_head;
 
 	argv = xcalloc(argv_len(tok) + 1, sizeof(char *));
 	i = 0;
 	while (tok && !at_eof(tok))
 	{
 		if (tok->is_expanded)
-		{
-			is_word_head = true;
-			j = 0;
-			while (j < ft_strlen(tok->word))
-			{
-				if (ft_isspace(tok->word[j]))
-					is_word_head = true;
-				else if (is_word_head)
-				{
-					is_word_head = false;
-					argv[i] = dup_until_space(tok->word + j);
-					i++;
-				}
-				j++;
-			}
-		}
+			argv = dup_is_expanded(argv, i, tok);
 		else
 			argv[i] = xstrdup(tok->word);
 		i++;
