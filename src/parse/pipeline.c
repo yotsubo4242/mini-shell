9a76687abb8f78b83bd0fa7e68597040a854c65f
yotsubo <513047.otsubo@gmail.com>
2024-12-13 17:54:23 +0900

    [#125] ref: pipeline.c: adjust to norm

--- src/parse/pipeline.c
+++ src/parse/pipeline.c
@@ -6,33 +6,11 @@
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:34:14 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/12/13 17:44:08 by yuotsubo         ###   ########.fr       */
+/*   Updated: 2024/12/13 17:51:46 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static bool	is_control_operator_exit(bool res, char **operators)
-{
-	free_argv(operators);
-	return (res);
-}
-
-bool	is_control_operator(t_token *tok)
-{
-	char	**operators;
-	size_t	i;
-
-	operators = make_operators();
-	i = 0;
-	while (i < sizeof(operators) / sizeof(*operators))
-	{
-		if (startswith(tok->word, operators[i]))
-			return (is_control_operator_exit(true, operators));
-		i++;
-	}
-	return (is_control_operator_exit(false, operators));
-}
 
 void	append_command_element(t_node *cmd, t_token **rest, t_token *tok)
 {
