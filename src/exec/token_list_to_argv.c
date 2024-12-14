3dfcf1bd710ed461316161ac1ae63609dfcdeb23
yotsubo <513047.otsubo@gmail.com>
2024-12-14 17:11:56 +0900

    [#150] modified: token_list_to_argv.c: delete trush if

--- src/exec/token_list_to_argv.c
+++ src/exec/token_list_to_argv.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   interpret.c                                        :+:      :+:    :+:   */
+/*   token_list_to_argv.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 02:34:47 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/12/13 15:54:19 by tkitahar         ###   ########.fr       */
+/*   Updated: 2024/12/14 17:10:52 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,7 @@ char	**token_list_to_argv(t_token *tok)
 	i = 0;
 	while (tok && !at_eof(tok))
 	{
-		if (tok->is_expanded)
-			argv[i] = dup_is_expanded(argv, i, tok);
-		else
-			argv[i] = xstrdup(tok->word);
+		argv[i] = xstrdup(tok->word);
 		i++;
 		tok = tok->next;
 	}
