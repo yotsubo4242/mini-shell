55b06691c79257b6ad03f0cb52f38cc37339184b
yotsubo <513047.otsubo@gmail.com>
2024-12-14 14:00:06 +0900

    [#112] modified: builtin_export.c: print sorted env in builtin_export()

--- include/prototype.h
+++ include/prototype.h
@@ -6,7 +6,7 @@
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:29:07 by yuotsubo          #+#    #+#             */
-/*   Updated: 2024/12/14 13:13:51 by yuotsubo         ###   ########.fr       */
+/*   Updated: 2024/12/14 13:49:42 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ long	ft_strtol(const char *str);
 
 // env
 bool	is_identifier(const char *str);
+t_item	*item_new(const char *key, const char *value);
 void	item_update(t_item *item, const char *value, bool should_free);
 void	item_apend(const char *key, const char *value);
 void	cleanup_map(t_map *map);
--- src/builtin/builtin_export.c
+++ src/builtin/builtin_export.c
@@ -6,17 +6,64 @@
 /*   By: yuotsubo <yuotsubo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 16:15:23 by yotsubo           #+#    #+#             */
-/*   Updated: 2024/12/12 18:12:59 by yuotsubo         ###   ########.fr       */
+/*   Updated: 2024/12/14 13:48:33 by yuotsubo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	item_apend_acending(const char *key, const char *value, t_map *new_env)
+{
+	t_item	*item;
+	t_item	*new;
+	t_item	*prev;
+
+	item = &(new_env->item_head);
+	if (value == NULL)
+		new = item_new(key, NULL);
+	else
+		new = item_new(key, value);
+	if (item->next == NULL)
+		item->next = new;
+	else
+	{
+		prev = item;
+		item = item->next;
+		while (item != NULL && ft_strcmp(key, item->key) > 0)
+		{
+			prev = item;
+			item = item->next;
+		}
+		prev->next = new;
+		new->next = item;
+	}
+}
+
+static t_map	*make_sort_env(t_map *env)
+{
+	t_map	*new_env;
+	t_item	*item;
+
+	new_env = map_new();
+	item = env->item_head.next;
+	while (item)
+	{
+		if (new_env->item_head.next == NULL)
+			new_env->item_head.next = item_new(item->key, item->value);
+		else
+			item_apend_acending(item->key, item->value, new_env);
+		item = item->next;
+	}
+	return (new_env);
+}
+
 static void	print_allenv(void)
 {
 	t_item	*item;
+	t_map	*env;
 
-	item = gs_env(GET, NULL)->item_head.next;
+	env = make_sort_env(gs_env(GET, NULL));
+	item = env->item_head.next;
 	while (item)
 	{
 		if (item->value)
@@ -25,6 +72,7 @@ static void	print_allenv(void)
 			ft_printf("declare -x %s\n", item->key);
 		item = item->next;
 	}
+	free_map(env);
 }
 
 int	builtin_export(char **argv)
